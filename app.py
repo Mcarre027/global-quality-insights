202ee83dfb6fb7789a09c24d8fbaef104e490717
Mcarre027 <carrehomebusiness@gmail.com>
2025-04-17 15:29:23 +0200

    fix build problem

--- app.py
+++ app.py
@@ -72,4 +72,9 @@ def display_page(pathname, lang):
 callbacks.register_callbacks(app)
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False,
+        use_reloader=False)
