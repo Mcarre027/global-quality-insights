dccf4818c5c2d8699b12bd463ed74e220ba2d052
Mcarre027 <carrehomebusiness@gmail.com>
2025-04-17 17:03:07 +0200

    changement de port

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def display_page(pathname, lang):
 callbacks.register_callbacks(app)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8050))
+    port = int(os.environ.get("PORT", 8080))
     app.run(
         host="0.0.0.0",
         port=port,
